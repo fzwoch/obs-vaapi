30ab1af4791e87d8ba25ba65d21c143297c3d898
Florian Zwoch <fzwoch@gmail.com>
2022-08-01 16:57:54 +0200

    wip

--- obs-vaapi.c
+++ obs-vaapi.c
@@ -1,11 +1,14 @@
 #include <obs-module.h>
 #include <gst/gst.h>
+#include <gst/app/app.h>
 
 OBS_DECLARE_MODULE()
 
 typedef struct {
 	obs_encoder_t *encoder;
 	GstElement *pipe;
+	GstElement *appsrc;
+	GstElement *appsink;
 } obs_vaapi_t;
 
 static const char *get_name(void *type_data)
@@ -20,8 +23,9 @@ static void *create(obs_data_t *settings, obs_encoder_t *encoder)
 	vaapi->encoder = encoder;
 
 	GError *err = NULL;
-	vaapi->pipe =
-		gst_parse_launch("vaapih264enc name=encoder ! fakesink", &err);
+	vaapi->pipe = gst_parse_launch(
+		"appsrc name=appsrc ! vaapih264enc name=encoder ! h264parse ! video/x-h264, stream-format=byte-stream, alignment=au ! appsink sync=false name=appsink",
+		&err);
 	if (err) {
 		blog(LOG_ERROR, "[obs-vaapi] %s", err->message);
 
@@ -37,8 +41,6 @@ static void *create(obs_data_t *settings, obs_encoder_t *encoder)
 	for (obs_property_t *property =
 		     obs_properties_first(obs_encoder_properties(encoder));
 	     property; obs_property_next(&property)) {
-
-		blog(LOG_INFO, "%s", (obs_property_name(property)));
 
 		switch (obs_property_get_type(property)) {
 		case OBS_PROPERTY_TEXT:
@@ -78,6 +80,19 @@ static void *create(obs_data_t *settings, obs_encoder_t *encoder)
 
 	gst_object_unref(element);
 
+	vaapi->appsrc = gst_bin_get_by_name(GST_BIN(vaapi->pipe), "appsrc");
+	vaapi->appsink = gst_bin_get_by_name(GST_BIN(vaapi->pipe), "appsink");
+
+	GstCaps *caps = gst_caps_new_simple(
+		"video/x-raw", "format", G_TYPE_STRING, "NV12", "framerate",
+		GST_TYPE_FRACTION, 0, 1, "width", G_TYPE_INT,
+		obs_encoder_get_width(encoder), "height", G_TYPE_INT,
+		obs_encoder_get_height(encoder), "interlace-mode",
+		G_TYPE_STRING, "progressive", NULL);
+
+	g_object_set(vaapi->appsrc, "caps", caps, NULL);
+
+	gst_caps_unref(caps);
 	gst_element_set_state(vaapi->pipe, GST_STATE_PLAYING);
 
 	return vaapi;
@@ -89,6 +104,8 @@ static void destroy(void *data)
 
 	if (vaapi->pipe) {
 		gst_element_set_state(vaapi->pipe, GST_STATE_NULL);
+		gst_object_unref(vaapi->appsink);
+		gst_object_unref(vaapi->appsrc);
 		gst_object_unref(vaapi->pipe);
 	}
 
@@ -99,6 +116,55 @@ static bool encode(void *data, struct encoder_frame *frame,
 		   struct encoder_packet *packet, bool *received_packet)
 {
 	obs_vaapi_t *vaapi = (obs_vaapi_t *)data;
+
+	GstBuffer *buffer = gst_buffer_new_wrapped_full(
+		0, frame->data[0],
+		obs_encoder_get_width(vaapi->encoder) *
+			obs_encoder_get_width(vaapi->encoder) * 3 / 2,
+		0,
+		obs_encoder_get_width(vaapi->encoder) *
+			obs_encoder_get_width(vaapi->encoder) * 3 / 2,
+		NULL, NULL);
+
+	//	GST_BUFFER_PTS(buffer) =
+	//		frame->pts *
+	//		(GST_SECOND / (data->ovi.fps_num / data->ovi.fps_den));
+
+	gst_app_src_push_buffer(GST_APP_SRC(vaapi->appsrc), buffer);
+
+	GstSample *sample =
+		gst_app_sink_try_pull_sample(GST_APP_SINK(vaapi->appsink), 0);
+	if (sample == NULL) {
+		return true;
+	}
+
+	*received_packet = true;
+
+	buffer = gst_sample_get_buffer(sample);
+
+	GstMapInfo info;
+
+	gst_buffer_map(buffer, &info, GST_MAP_READ);
+
+	packet->data = info.data; //FIXME
+	packet->size = info.size;
+
+	gst_buffer_unmap(buffer, &info);
+
+	packet->pts = GST_BUFFER_PTS(buffer);
+	packet->dts = GST_BUFFER_DTS(buffer);
+
+	packet->pts /=
+		GST_SECOND / (packet->timebase_den / packet->timebase_num);
+	packet->dts /=
+		GST_SECOND / (packet->timebase_den / packet->timebase_num);
+
+	packet->type = OBS_ENCODER_VIDEO;
+
+	packet->keyframe =
+		!GST_BUFFER_FLAG_IS_SET(buffer, GST_BUFFER_FLAG_DELTA_UNIT);
+
+	gst_sample_unref(sample);
 
 	return true;
 }
@@ -264,11 +330,18 @@ static obs_properties_t *get_properties(void *data)
 				property, g_param_spec_get_blurb(param));
 			break;
 		case G_TYPE_FLOAT:
-		case G_TYPE_DOUBLE:
 			property = obs_properties_add_float(
 				properties, param->name, param->name,
 				G_PARAM_SPEC_FLOAT(param)->minimum,
-				G_PARAM_SPEC_FLOAT(param)->maximum, 1.0);
+				G_PARAM_SPEC_FLOAT(param)->maximum, 0.1);
+			obs_property_set_long_description(
+				property, g_param_spec_get_blurb(param));
+			break;
+		case G_TYPE_DOUBLE:
+			property = obs_properties_add_float(
+				properties, param->name, param->name,
+				G_PARAM_SPEC_DOUBLE(param)->minimum,
+				G_PARAM_SPEC_DOUBLE(param)->maximum, 0.1);
 			obs_property_set_long_description(
 				property, g_param_spec_get_blurb(param));
 			break;
